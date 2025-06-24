dde50d05d5b6fc4a23bd1c5c220cac7f58b7085e
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-06-24 17:16:57 -0300

    Commit

---
+++ src/je_06_operadores/Atribuicoes.java
@@ -0,0 +1,15 @@
+package je_06_operadores;
+
+import java.util.Date;
+
+public class Atribuicoes {
+    public static void main(String[] args) {
+        String nome = "Gabriel";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+        System.out.println(nome);
+    }
+}
